e1c67976015341dcdb7923f42b6ebb9e4d5b5275
omarmansour557 <omarmansour557@gmail.com>
2022-03-10 11:30:01 +0200

    update files

---
+++ src/equalization.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtCore as qtc 
+from PyQt5 import QtWidgets as qtw 
+from PyQt5 import uic
+
+class Equalizer (qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        uic.loadUi("src/ui/equalization.ui",self)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print('Welcome')
+from PyQt5 import QtCore as qtc 
+from PyQt5 import QtWidgets as qtw 
+from PyQt5 import uic
+import sys
+import qdarkstyle
+
+from equalization import Equalizer
+
+class MainWindow(qtw.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi("src/ui/mainWindow.ui", self)
+        self.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+        self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        self.equalizer = Equalizer()
+        self.centralWidget().layout().addWidget(self.equalizer)
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
