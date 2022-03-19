0b0178f69d4bc208029ed833acd0c98b66b9e886
omarmansour557 <omarmansour557@gmail.com>
2022-03-19 18:16:07 +0200

    fix bug in load_image_function

--- src/equalization.py
+++ src/equalization.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets as qtw 
+from PyQt5 import QtWidgets as qtw
 from PyQt5 import uic
 import cv2
 import numpy as np
@@ -6,10 +6,10 @@ from viewer import Viewer
 
 
 class Equalizer(qtw.QWidget):
-    
+
     def __init__(self):
         super().__init__()
-        
+
         uic.loadUi("src/ui/equalization.ui", self)
 
         self.init_gui()
@@ -28,23 +28,26 @@ class Equalizer(qtw.QWidget):
 
         self.equalizer_btn.clicked.connect(self.equalization)
         self.histogram_btn.clicked.connect(self.view_histogram)
-    
+
     def init_gui(self):
         self.img = [[]]
         self.equalized_image = [[]]
 
-
     def load_original_image(self, image_path):
         self.img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         self.view_original_image()
+        self.equalized_viewer.clear_canvans()
+        self.histogram_viewer.clear_canvans()
+        self.equalized_histogram.clear_canvans()
 
     def equalization(self):
-        _, indices, counts = np.unique(self.img, return_counts=True, return_inverse=True)
-        
+        _, indices, counts = np.unique(
+            self.img, return_counts=True, return_inverse=True)
+
         # equalization factor: pixel depth divided by number of pixels of image.
         equalization_factor = 2**8/(self.img.shape[0]*self.img.shape[1])
-        
-        commulative_density_function = []   
+
+        commulative_density_function = []
 
         # to calculate commulative density function (cdf)
         commulative_density_function = np.cumsum(counts)
@@ -53,18 +56,18 @@ class Equalizer(qtw.QWidget):
 
         # after equalization --> pixels intensity may have float number ---> this reason for calculate ceiling of number
         Sx = np.ceil(Sx)
-      
+
         image_1D = Sx
 
         # to reverse the origin 1d array after equalization
         image_1D = image_1D[indices]
 
         # to return to original image
-        self.equalized_image = np.reshape(image_1D, (self.img.shape[0],self.img.shape[1]))
+        self.equalized_image = np.reshape(
+            image_1D, (self.img.shape[0], self.img.shape[1]))
 
-        
         self.view_equalized_image()
-        
+
     def view_original_image(self):
         self.image_viewer.draw_image(self.img)
 
