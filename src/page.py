ee9bbd8a8854820a486c59adf1a1eedb9bb3f75e
saied-salem <saied.salem44@gmail.com>
2022-05-30 00:26:22 +0200

    solving overwrite problems

--- src/page.py
+++ src/page.py
@@ -5,12 +5,13 @@ from PyQt5 import uic
 
 from filtering_studio import FilterStudio
 from equalization import Equalizer
+from boundry_tracing import Boundry_tracing
 
 class Page (qtw.QTabWidget):
     def __init__ (self):
         super().__init__()
 
-        uic.loadUi("src/ui/page.ui", self)
+        uic.loadUi("ui/page.ui", self)
 
         
         self.filter_studio = FilterStudio()
@@ -19,6 +20,9 @@ class Page (qtw.QTabWidget):
 
         self.filter_layout.addWidget(self.filter_studio)
         self.equalizer_layout.addWidget(self.equalizer)
+
+        self.bourder_trecer= Boundry_tracing()
+        self.Boarder_tracing_layout.addWidget(self.bourder_trecer)
 
     def load_image(self, image_path):
         self.filter_studio.load_original_image(image_path)
